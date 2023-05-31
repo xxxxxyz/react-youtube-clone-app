a26044a3d5e6442302454d100f35fb1092afe4fd
yuzexia <xiayuze0406@gmail.com>
2023-05-31 22:50:59 +1000

    Setup Project

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Navbar } from "./Navbar";
+export { default as VideoDetail } from "./VideoDetail";
+export { default as ChannelDetail } from "./ChannelDetail";
+export { default as } from "./";
+export { default as Navbar } from "./Navbar";
