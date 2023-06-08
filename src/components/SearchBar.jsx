8548ebf65c2619e5a8c0f5c0e5facee9117a8fe0
yuzexia <xiayuze0406@gmail.com>
2023-06-08 21:32:15 +1000

    Initialize Search Bar and  Video Detail

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,12 +4,26 @@ import { Paper, IconButton } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 
 const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (searchTerm) {
+      navigate(`/search/${searchTerm}`);
+
+      setSearchTerm("");
+    }
+  };
+
   return (
     <Paper
       component="form"
-      onSubmit={() => {}}
+      onSubmit={handleSubmit}
       sx={{
-        borderRadius: "1px solid #e3e3e3",
+        borderRadius: 20,
+        border: "1px solid #e3e3e3",
         pl: 2,
         boxShadow: "none",
         mr: { sm: 5 },
@@ -18,10 +32,14 @@ const SearchBar = () => {
       <input
         className="search-bar"
         placeholder="Search..."
-        value=""
-        onChange={() => {}}
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
       />
-      <IconButton type="submit" sx={{ p: "10px", color: "red" }}>
+      <IconButton
+        type="submit"
+        sx={{ p: "10px", color: "red" }}
+        aria-label="search"
+      >
         <SearchIcon />
       </IconButton>
     </Paper>
